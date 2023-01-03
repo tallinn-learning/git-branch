0b3bfe412364c6b7926d7ccec18b90da388ab1cd
vladimir.ovodenko <vladimir.ovodenko@gamesys.com>
2023-01-03 16:18:32 +0200

    one only

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,11 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("branch one created");
-
-        System.out.println( "Hello World!" );
-
-        System.out.println("branch two created");
-        System.out.println("commit to master");
+        System.out.println(" one only m");
     }
 }
